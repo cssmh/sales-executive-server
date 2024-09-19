d678a6f621129c75b5421a85e3b27d52c4b8fbcf
cssmh <touristmomen@gmail.com>
2024-09-19 23:29:42 +0600

    added middleware, cors, all get, post delete routes

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 require("dotenv").config();
 const port = 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://saleproducts-aff1e.web.app",
+      "https://sellproduct.netlify.app",
+    ],
+  })
+);
 app.use(express.json());
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,12 +27,65 @@ const client = new MongoClient(process.env.URI, {
 
 async function run() {
   try {
-    client.connect();
+    // client.connect();
+    const OrderCollection = client.db("OrderManagement").collection("Orders");
+
+    app.get("/all-order", async (req, res) => {
+      try {
+        const result = await OrderCollection.find().toArray();
+        res.send(result);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+    app.get("/order/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await OrderCollection.findOne(query);
+        res.send(result);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+    app.get("/my-orders", async (req, res) => {
+      try {
+        const email = req.query.email;
+        let query = {};
+        if (email) {
+          query = { seller: email };
+        }
+        const result = await OrderCollection.find(query).toArray();
+        res.send(result);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+    app.post("/add-order", async (req, res) => {
+      try {
+        const result = await OrderCollection.insertOne(req.body);
+        res.send(result);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+    app.delete("/order/:id", async (req, res) => {
+      try {
+        const query = { _id: new ObjectId(req.params?.id) };
+        const result = await OrderCollection.deleteOne(query);
+        res.send(result);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    console.log("Pinged your deployment. Successfully connected to MongoDB!");
   } finally {
     // await client.close();
   }
